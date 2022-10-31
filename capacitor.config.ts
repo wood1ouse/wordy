e42b2db7b91f1edb91ed11e6ec05032a77b9bedd
Woodlouse <m.mamontov-2024@kpi.ua>
2022-10-31 23:38:42 +0200

    project set up

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'wordy',
+  webDir: 'dist/wordy',
+  bundledWebRuntime: false
+};
+
+export default config;
